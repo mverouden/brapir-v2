3f08eeed423e6eef8e3288fdd037b384bac18c38
Verouden, Maikel <maikel.verouden@gmail.com>
2021-02-05 15:50:31 +0100

    first effort to fill in brapi_results2df()

--- R/brapi_result2df.R
+++ R/brapi_result2df.R
@@ -2,15 +2,15 @@
 ### a flattened data.frame object
 ###
 ### resultList can consist of:
-### 1) Single Response: In this type of response, the result field is an object
+### 1) Single Response (master): In this type of response, the result field is an object
 ###    describing a single record of data. There should NOT be a "data" array/element
 ###    in this case and pagination does not apply. A Single Response is used when
 ###    you are requesting or modifying a single, specific data object by its DbId.
-### 2) List Response: In this type of response, the result object only contains
+### 2) List Response (detail): In this type of response, the result object only contains
 ###    a "data" array/element, which is an arbitrarily long array of objects of
 ###    the same type. Pagination request parameters and pagination metadata
 ###    fields apply to this data array.
-### 3) Special cases: mix of Single and List response (parse "data" element and
+### 3) Special cases (master/detail: mix of Single and List response (parse "data" element and
 ###    repeat single response part to match dimensions).
 #' @importFrom utils as.relistable read.csv read.delim
 brapi_result2df <- function(cont, usedArgs) {
@@ -19,5 +19,263 @@ brapi_result2df <- function(cont, usedArgs) {
   contList <- jsonlite::fromJSON(txt = cont)
   ## Use only the result element from the content list (contList)
   resultList <- contList[["result"]]
-
+  if ("data" %in% names(resultList)) {
+    payload <- ifelse(test = length(resultList) == 1,
+                      yes = "detail",
+                      no =  "master/detail")
+  } else {
+    payload <- "master"
+  }
+  switch(payload,
+         "master" = {
+           if (all(lengths(resultList) <= 1)) {
+             ## use only lengths == 1
+             master <- as.data.frame(resultList[lengths(resultList) == 1],
+                                     stringsAsFactors = FALSE)
+             dat <- master
+           } else {
+             master <- as.data.frame(resultList[lengths(resultList) == 1],
+                                     stringsAsFactors = FALSE)
+             tempmaster <- list()
+             for (l1name in names(which(lengths(resultList) > 1))) {
+               switch(class(resultList[[l1name]]),
+                      "character" = {
+                        tempmaster[[l1name]] <- paste(resultList[[l1name]],
+                                                      collapse = ", ")
+                      },
+                      "data.frame" = {
+                        for (i in seq_len(nrow(resultList[[l1name]]))) {
+                          for (j in seq_along(resultList[[l1name]])) {
+                            tempmaster[[paste(l1name,
+                                              colnames(resultList[[l1name]][j]),
+                                              i,
+                                              sep = ".")]] <- resultList[[l1name]][i, j]
+                          }
+                        }
+                      },
+                      "list" = {
+                        templist <- as.list(data.frame(t(as.matrix(unlist(as.relistable(resultList[[l1name]])))),
+                                                       stringsAsFactors = FALSE))
+                        names(templist) <- paste(l1name, names(templist), sep = ".")
+                        tempmaster <- c(tempmaster, templist)
+                      })
+             }
+             if (length(master) == 0) {
+               dat <- as.data.frame(tempmaster, stringsAsFactors = FALSE)
+             } else {
+               dat <- cbind(master, as.data.frame(tempmaster, stringsAsFactors = FALSE))
+             }
+           }
+         },
+         "detail" = {
+           if (inherits(resultList[["data"]], what = "data.frame")) {
+             detail <- as.data.frame(x = jsonlite::flatten(resultList[["data"]],
+                                                           recursive = TRUE),
+                                     stringsAsFactors = FALSE)
+           } else {
+             detail <- as.data.frame(x = resultList[["data"]],
+                                     stringsAsFactors = FALSE)
+           }
+           namesListCols <- names(which(sapply(X = detail,
+                                               FUN = inherits,
+                                               what = "list")))
+           for (colName in namesListCols) {
+             ## list of data.frame
+             if (all(sapply(X = detail[[colName]],
+                            FUN = inherits,
+                            what = "data.frame"))) {
+               ## Determine rows with non-empty data.frames
+               nonEmptyDetailRows <- which(lengths(detail[[colName]]) != 0)
+               ## Create an empty data.frame with columnnames
+               tempColNames <- names(jointDetail(detail[nonEmptyDetailRows[1], ], colName))
+               tempDetail <- data.frame(matrix(ncol = length(tempColNames), nrow = 0))
+               names(tempDetail) <- tempColNames
+               nrows <- nrow(detail)
+               if (nrows >= 1) {
+                 for (i in seq_len(nrows)) {
+                   if (i %in% nonEmptyDetailRows) {
+                     nextRow <- jointDetail(detail[i, ], colName)
+                     if (nrow(tempDetail) == 0) {
+                       tempDetail <- nextRow
+                     } else {
+                       tempDetail <- dplyr::bind_rows(tempDetail, nextRow)
+                     }
+                     rm(nextRow)
+                   } else {# i %in% emptyDetailRows
+                     partRowDetail <- detail[i, -c(which(names(detail) == colName))]
+                     extColNames <- setdiff(names(tempDetail), names(partRowDetail))
+                     partRowExt <- data.frame(matrix(data = NA, nrow = 1, ncol = length(extColNames)))
+                     names(partRowExt) <- extColNames
+                     nextRow <- dplyr::bind_cols(partRowDetail, partRowExt)
+                     if (nrow(tempDetail) == 0) {
+                       tempDetail <- nextRow
+                     } else {
+                       tempDetail <- dplyr::bind_rows(tempDetail, nextRow)
+                     }
+                     rm(partRowDetail, partRowExt, extColNames, nextRow)
+                   }
+                 }
+               }
+               tempDetail[[colName]] <- NULL
+               ## Remove duplicated rows
+               tempDetail <- tempDetail[!duplicated(tempDetail), ]
+               ## Row renumbering
+               rownames(tempDetail) <- seq_len(nrow(tempDetail))
+             }
+             ## list of character
+             if (all(sapply(X = detail[[colName]],
+                            FUN = inherits,
+                            what = "character"))) {
+               detail[[colName]] <- sapply(X = detail[[colName]],
+                                           FUN = paste,
+                                           collapse = "; ")
+             }
+             ## list of list
+             if (all(sapply(X = detail[[colName]],
+                            FUN = inherits, what = "list"))) {
+               if (all(lengths(detail[[colName]]) == 0)) {
+                 ## list of empty lists
+                 detail[[colName]] <- NULL
+               }
+             }
+             if (exists("tempDetail")) {
+               detail <- tempDetail
+               rm(tempDetail)
+             }
+           }
+           dat <- detail
+         },
+         "master/detail" = {##headerRow! e.g. /search/observationtables/{searchResultsDbId}
+           ## Process master part
+           masterList <- resultList[!names(resultList) %in% "data"]
+           if (all(c("headerRow", "observationVariableDbIds", "observationVariableNames") %in% names(masterList))) {
+             headerRow <- c(masterList[["headerRow"]],
+                            paste(masterList[["observationVariableNames"]],
+                                  masterList[["observationVariableDbIds"]],
+                                  sep = "|"))
+           } else {
+             if (!all(lengths(masterList) <= 1)) {
+               master <- masterList[lengths(masterList) == 1]
+               tempmaster <- list()
+               for (l1name in names(which(lengths(masterList) > 1))) {
+                 switch(class(masterList[[l1name]]),
+                        "character" = {
+                          tempmaster[[l1name]] <- paste(masterList[[l1name]],
+                                                        collapse = ", ")
+                        },
+                        "data.frame" = {
+                          for (i in seq_len(nrow(masterList[[l1name]]))) {
+                            for (j in seq_along(masterList[[l1name]])) {
+                              tempmaster[[paste(l1name,
+                                                colnames(masterList[[l1name]][j]),
+                                                i,
+                                                sep = ".")]] <- masterList[[l1name]][i, j]
+                            }
+                          }
+                        },
+                        "list" = {
+                          templist <- as.list(data.frame(t(as.matrix(unlist(as.relistable(masterList[[l1name]])))),
+                                                         stringsAsFactors = FALSE))
+                          names(templist) <- paste(l1name, names(templist), sep = ".")
+                          tempmaster <- c(tempmaster, templist)
+                        })
+               }
+               if (length(master) == 0) {
+                 master <- tempmaster
+               } else {
+                 master <- c(master, tempmaster)
+               }
+             } else {
+               master <-  masterList[lengths(masterList) == 1]
+             }
+           }
+           ## Process detail part
+           if (inherits(resultList[["data"]], what = "data.frame")) {
+             detail <- as.data.frame(x = jsonlite::flatten(resultList[["data"]],
+                                                           recursive = TRUE),
+                                     stringsAsFactors = FALSE)
+           } else {
+             detail <- as.data.frame(x = resultList[["data"]],
+                                     stringsAsFactors = FALSE)
+           }
+           if (exists("headerRow")) {
+             colnames(detail) <- headerRow
+             namesListCols <- names(detail)
+           } else {
+             namesListCols <- names(which(sapply(X = detail,
+                                                 FUN = inherits,
+                                                 what = "list")))
+           }
+           for (colName in namesListCols) {
+             ## list of data.frame
+             if (all(sapply(X = detail[[colName]],
+                            FUN = inherits,
+                            what = "data.frame"))) {
+               ## Determine rows with non-empty data.frames
+               nonEmptyDetailRows <- which(lengths(detail[[colName]]) != 0)
+               ## Create an empty data.frame with columnnames
+               tempColNames <- names(jointDetail(detail[nonEmptyDetailRows[1], ], colName))
+               tempDetail <- data.frame(matrix(ncol = length(tempColNames), nrow = 0))
+               names(tempDetail) <- tempColNames
+               nrows <- nrow(detail)
+               if (nrows >= 1) {
+                 for (i in seq_len(nrows)) {
+                   if (i %in% nonEmptyDetailRows) {
+                     nextRow <- jointDetail(detail[i, ], colName)
+                     if (nrow(tempDetail) == 0) {
+                       tempDetail <- nextRow
+                     } else {
+                       tempDetail <- dplyr::bind_rows(tempDetail, nextRow)
+                     }
+                     rm(nextRow)
+                   } else {# i %in% emptyDetailRows
+                     partRowDetail <- detail[i, -c(which(names(detail) == colName))]
+                     extColNames <- setdiff(names(tempDetail), names(partRowDetail))
+                     partRowExt <- data.frame(matrix(data = NA, nrow = 1, ncol = length(extColNames)))
+                     names(partRowExt) <- extColNames
+                     nextRow <- dplyr::bind_cols(partRowDetail, partRowExt)
+                     if (nrow(tempDetail) == 0) {
+                       tempDetail <- nextRow
+                     } else {
+                       tempDetail <- dplyr::bind_rows(tempDetail, nextRow)
+                     }
+                     rm(partRowDetail, partRowExt, extColNames, nextRow)
+                   }
+                 }
+               }
+               tempDetail[[colName]] <- NULL
+               ## Remove duplicated rows
+               tempDetail <- tempDetail[!duplicated(tempDetail), ]
+               ## Row renumbering
+               rownames(tempDetail) <- seq_len(nrow(tempDetail))
+             }
+             ## list of character
+             if (all(sapply(X = detail[[colName]],
+                            FUN = inherits,
+                            what = "character"))) {
+               detail[[colName]] <- sapply(X = detail[[colName]],
+                                           FUN = paste,
+                                           collapse = "; ")
+             }
+             ## list of list
+             if (all(sapply(X = detail[[colName]],
+                            FUN = inherits,
+                            what = "list"))) {
+               if (all(lengths(detail[[colName]]) == 0)) {
+                 ## list of empty lists
+                 detail[[colName]] <- NULL
+               }
+             }
+             if (exists("tempDetail")) {
+               detail <- tempDetail
+               rm(tempDetail)
+             }
+           }
+           if (exists("headerRow")) {
+             dat <- detail
+           } else {
+             dat <- cbind(as.data.frame(master, stringsAsFactors = FALSE), detail)
+           }
+         })
+  return(dat)
 }
