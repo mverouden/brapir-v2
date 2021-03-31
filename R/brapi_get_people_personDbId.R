19f54ad069199e8f933e1f8d469004ed239a9ab9
Verouden, Maikel <maikel.verouden@gmail.com>
2021-03-31 13:06:37 +0200

    added manual page for brapi_get_people_personDbId()

---
+++ R/brapi_get_people_personDbId.R
@@ -0,0 +1,54 @@
+#' @title
+#' get /people/\{personDbId\}
+#'
+#' @description
+#' Get the details for a specific Person
+#'
+#' @param con list; required: TRUE; BrAPI connection object
+#' @param personDbId character; required: TRUE; The unique ID of a person
+#'
+#' @details Get the details for a specific Person
+#'
+#' @return data.frame
+#'
+#' @author Maikel Verouden
+#'
+#' @references \href{https://app.swaggerhub.com/apis/PlantBreedingAPI/BrAPI-Core/2.0#/People/get_people__personDbId_ }{BrAPI SwaggerHub}
+#'
+#' @family brapi-core
+#' @family People
+#'
+#' @examples
+#' con <- brapi_db()$testserver
+#' brapi_get_people_personDbId(con = con, personDbId = "person1")
+#' }
+#'
+#' @export
+brapi_get_people_personDbId <- function(con = NULL, personDbId = '') {
+  ## Create a list of used arguments
+  usedArgs <- brapir:::brapi_usedArgs(origValues = FALSE)
+  ## Check if BrAPI server can be reached given the connection details
+  brapi_checkCon(con = usedArgs[["con"]], verbose = FALSE)
+  ## Check validity of used and required arguments
+  brapir:::brapi_checkArgs(usedArgs, reqArgs = "personDbId")
+  ## Obtain the call url
+  callurl <- brapir:::brapi_GET_callURL(usedArgs = usedArgs,
+                                        callPath = "/people/{personDbId}",
+                                        reqArgs = "personDbId",
+                                        packageName = "BrAPI-Core",
+                                        callVersion = 2.0)
+
+  try({
+    ## Make the call and receive the response
+    resp <- brapir:::brapi_GET(url = callurl, usedArgs = usedArgs)
+    ## Extract the content from the response object in human readable form
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    ## Convert the content object into a data.frame
+    out <- brapir:::brapi_result2df(cont, usedArgs)
+  })
+  ## Set class of output
+  class(out) <- c(class(out), "brapi_get_people_personDbId")
+  ## Show pagination information from metadata
+  brapir:::brapi_serverinfo_metadata(cont)
+  return(out)
+}
