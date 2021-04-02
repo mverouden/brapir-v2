9992f5eac05241659e66250146dda0ac0459af47
Verouden, Maikel <maikel.verouden@gmail.com>
2021-04-02 12:32:55 +0200

    added brap_get_studytypes()

---
+++ R/brapi_get_studytypes.R
@@ -0,0 +1,57 @@
+#' @title
+#' get /studytypes
+#'
+#' @description
+#' Get the Study Types
+#'
+#' @param con list; required: TRUE; BrAPI connection object
+#' @param page integer; required: FALSE; Used to request a specific page of data to be returned. The page indexing starts at 0 (the first page is 'page'= 0). Default is `0`.
+#' @param pageSize integer; required: FALSE; The size of the pages to be returned. Default is `1000`.
+#'
+#' @details Call to retrieve the list of study types.
+#'
+#' @return data.frame
+#'
+#' @author Maikel Verouden
+#'
+#' @references \href{https://app.swaggerhub.com/apis/PlantBreedingAPI/BrAPI-Core/2.0#/Studies/get_studytypes }{BrAPI SwaggerHub}
+#'
+#' @family brapi-core
+#' @family Studies
+#'
+#' @examples
+#' \dontrun{
+#' con <- brapi_db()$testserver
+#' brapi_get_studytypes(con = con)
+#' }
+#'
+#' @export
+brapi_get_studytypes <- function(con = NULL, page = 0, pageSize = 1000) {
+  ## Create a list of used arguments
+  usedArgs <- brapir:::brapi_usedArgs(origValues = FALSE)
+  ## Check if BrAPI server can be reached given the connection details
+  brapi_checkCon(con = usedArgs[["con"]], verbose = FALSE)
+  ## Check validity of used and required arguments
+  brapir:::brapi_checkArgs(usedArgs, reqArgs = "")
+  ## Obtain the call url
+  callurl <- brapir:::brapi_GET_callURL(usedArgs = usedArgs,
+                                        callPath = "/studytypes",
+                                        reqArgs = "",
+                                        packageName = "BrAPI-Core",
+                                        callVersion = 2.0)
+
+  try({
+    ## Make the call and receive the response
+    resp <- brapir:::brapi_GET(url = callurl, usedArgs = usedArgs)
+    ## Extract the content from the response object in human readable form
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    ## Convert the content object into a data.frame
+    out <- brapir:::brapi_result2df(cont, usedArgs)
+    colnames(out) <- "Study Types"
+  })
+  ## Set class of output
+  class(out) <- c(class(out), "brapi_get_studytypes")
+  ## Show pagination information from metadata
+  brapir:::brapi_serverinfo_metadata(cont)
+  return(out)
+}
