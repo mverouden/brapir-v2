c1df61e204bf563a9b2465dfce6132d536261b50
Verouden, Maikel <maikel.verouden@gmail.com>
2021-05-12 13:39:13 +0200

    added brapi_get_variables() with documentation and example

---
+++ R/brapi_get_variables.R
@@ -0,0 +1,79 @@
+#' @title
+#' get /variables
+#'
+#' @description
+#' Get the Observation Variables
+#'
+#' @param con list; required: TRUE; BrAPI connection object
+#' @param observationVariableDbId character; required: FALSE; Unique variable
+#'    identifier.
+#' @param traitClass character; required: FALSE; Variable's trait class
+#'    (phenological, physiological, morphological, _etc._).
+#' @param studyDbId character; required: FALSE; Unique study identifier to
+#'    filter on.
+#' @param externalReferenceID character; required: FALSE; An external reference
+#'    ID. Could be a simple string or a URI. (use with `externalReferenceSource`
+#'    parameter)
+#' @param externalReferenceSource character; required: FALSE; An identifier for
+#'    the source system or database of an external reference (use with
+#'    `externalReferenceID` parameter)
+#' @param page integer; required: FALSE; Used to request a specific page of data
+#'    to be returned. The page indexing starts at 0 (the first page is
+#'    `page = 0`). Default is `0`.
+#' @param pageSize integer; required: FALSE; The size of the pages to be
+#'    returned. Default is `1000`.
+#'
+#' @details Call to retrieve a list of observation variables available in the
+#'    database system.
+#'
+#' @return data.frame
+#'
+#' @author Maikel Verouden
+#'
+#' @references \href{https://app.swaggerhub.com/apis/PlantBreedingAPI/BrAPI-Phenotyping/2.0#/Observation%20Variables/get_variables }{BrAPI SwaggerHub}
+#'
+#' @family brapi-phenotyping
+#' @family Observation Variables
+#'
+#' @examples
+#' \dontrun{
+#' con <- brapi_db()$testserver
+#' brapi_get_variables(con = con)
+#' }
+#'
+#' @export
+brapi_get_variables <- function(con = NULL,
+                                observationVariableDbId = '',
+                                traitClass = '',
+                                studyDbId = '',
+                                externalReferenceID = '',
+                                externalReferenceSource = '',
+                                page = 0,
+                                pageSize = 1000) {
+  ## Create a list of used arguments
+  usedArgs <- brapirv2:::brapi_usedArgs(origValues = FALSE)
+  ## Check if BrAPI server can be reached given the connection details
+  brapi_checkCon(con = usedArgs[["con"]], verbose = FALSE)
+  ## Check validity of used and required arguments
+  brapirv2:::brapi_checkArgs(usedArgs, reqArgs = "")
+  ## Obtain the call url
+  callurl <- brapirv2:::brapi_GET_callURL(usedArgs = usedArgs,
+                                          callPath = "/variables",
+                                          reqArgs = "",
+                                          packageName = "BrAPI-Phenotyping",
+                                          callVersion = 2.0)
+
+  try({
+    ## Make the call and receive the response
+    resp <- brapirv2:::brapi_GET(url = callurl, usedArgs = usedArgs)
+    ## Extract the content from the response object in human readable form
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    ## Convert the content object into a data.frame
+    out <- brapirv2:::brapi_result2df(cont, usedArgs)
+  })
+  ## Set class of output
+  class(out) <- c(class(out), "brapi_get_variables")
+  ## Show pagination information from metadata
+  brapirv2:::brapi_serverinfo_metadata(cont)
+  return(out)
+}
