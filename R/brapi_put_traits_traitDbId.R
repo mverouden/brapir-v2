5f946d7e52719bda9bd7373edf6655a38e5f5b00
Verouden, Maikel <maikel.verouden@gmail.com>
2021-05-18 16:40:20 +0200

    added brapi_post_traits() and brap_put_traits_traitDbId() with documentation and working examples

---
+++ R/brapi_put_traits_traitDbId.R
@@ -0,0 +1,209 @@
+#' @title
+#' put /traits/\{traitDbId\}
+#'
+#' @description
+#' Update an existing Trait
+#'
+#' @param con list; required: TRUE; BrAPI connection object
+#' @param traitDbId character; required: TRUE; Identifier for the trait to
+#'    update details of.
+#' @param additionalInfo list; required: FALSE; Additional arbitrary information.
+#'    If provided use the following construct list(additionalProp1 = "string",
+#'    additionalProp2 =  "string", additionalProp3 = "string").
+#'
+#'    The Examples section shows an example on how to construct the
+#'    `additionalInfo` argument as a list.
+#' @param alternativeAbbreviations vector of type character; required: FALSE;
+#'    Other frequent abbreviations of the trait, if any. These abbreviations do
+#'    not have to follow a convention; default: &quot;&quot;, when using
+#'    multiple values supply as c(&quot;value1&quot;, &quot;value2&quot;).
+#' @param attribute character; required: FALSE; A trait can be decomposed as
+#'    &quot;Trait&quot; = &quot;Entity&quot; + &quot;Attribute&quot;, the
+#'    attribute is the observed feature (or characteristic) of the entity, e.g.
+#'    for &quot;grain colour&quot;, attribute = &quot;colour&quot;.
+#' @param entity character; required: FALSE; A trait can be decomposed as
+#'    &quot;Trait&quot; = &quot;Entity&quot; + &quot;Attribute&quot;, the entity
+#'    is the part of the plant that the trait refers to, e.g. for
+#'    &quot;grain colour&quot;, entity = &quot;grain&quot;.
+#' @param externalReferences data.frame; required: FALSE; A data.frame of
+#'    external reference ids. These are references to this piece of data in an
+#'    external system. Could be a simple string or a URI. The `externalReferences`
+#'    argument data.frame should contain the following columns:
+#'
+#'    * `referenceID` character; required: TRUE; The external reference ID. Could
+#'      be a simple string or a URI.
+#'    * `referenceSource` character; required: TRUE; An identifier for the source
+#'      system or database of this reference.
+#'
+#'    The Examples section shows an example of how to construct the
+#'    `externalReferences` argument as a data.frame.
+#' @param mainAbbreviation character; required: FALSE; Main abbreviation for
+#'    trait name, examples: &quot;Carotenoid content&quot; =&gt; &quot;CC&quot;).
+#' @param ontologyReference list; required: FALSE; MIAPPE V1.1 (DM-85) Variable
+#'    accession number - Accession number of the variable in the Crop Ontology.
+#'    (DM-87) Trait accession number - Accession number of the trait in a
+#'    suitable controlled vocabulary (Crop Ontology, Trait Ontology). (DM-89)
+#'    Method accession number - Accession number of the method in a suitable
+#'    controlled vocabulary (Crop Ontology, Trait Ontology). (DM-93) Scale
+#'    accession number - Accession number of the scale in a suitable controlled
+#'    vocabulary (Crop Ontology).
+#'
+#'   The `ontolgyReference` list should/can contain the following elements:
+#'
+#'   * `documentationLinks` data.frame; required: FALSE; Links to various
+#'     ontology documentation. Each row in the `documentationLinks` data.frame
+#'     should contain the following columns:
+#'
+#'       + `URL` character; required: FALSE; A uniform resource locator
+#'         specified as a valid uniform resource identifier (URI), e.g.
+#'         http://purl.obolibrary.org/obo/ro.owl
+#'       + `type` character; required: FALSE; one of &quot;OBO&quot;|
+#'         &quot;RDF&quot;|&quot;WEBPAGE&quot;.
+#'
+#'   * `ontologyDbId` character; required: TRUE; Ontology database unique
+#'     identifier.
+#'   * `ontologyName` character; required: TRUE; Ontology name, e.g. the Crop
+#'     Ontology.
+#'   * `version` character; required: FALSE; Ontology version (no specific
+#'     format).
+#'
+#'   The Examples section shows an example of how to construct the
+#'   `ontologyReference` argument as a list.
+#' @param status character; required: FALSE; Trait status (examples:
+#'    &quot;recommended&quot;, &quot;obsolete&quot;, &quot;legacy&quot;, *etc.*).
+#' @param synonyms vector of type character; required: FALSE; Other trait names;
+#'    default: &quot;&quot;, when using multiple values supply as c(
+#'    &quot;value1&quot;, &quot;value2&quot;).
+#' @param traitClass character; required: FALSE; Trait class, examples:
+#'    &quot;morphological&quot;, &quot;phenological&quot;,
+#'    &quot;agronomical&quot;, &quot;physiological&quot;,
+#'    &quot;abiotic stress&quot;, &quot;biotic stress&quot;,
+#'    &quot;biochemical&quot;, &quot;quality traits&quot;,
+#'    &quot;fertility&quot;, *etc.*).
+#' @param traitDescription character; required: FALSE; The description of a
+#'    trait.
+#' @param traitName character; required: FALSE; The human readable name of a
+#'    trait. MIAPPE V1.1 (DM-86) Trait - Name of the (plant or environmental)
+#'    trait under observation.
+#'
+#' @details Update an existing trait
+#'
+#' @return data.frame
+#'
+#' @author Maikel Verouden
+#'
+#' @references \href{https://app.swaggerhub.com/apis/PlantBreedingAPI/BrAPI-Phenotyping/2.0#/Traits/put_traits__traitDbId_ }{BrAPI SwaggerHub}
+#'
+#' @family brapi-phenotyping
+#' @family Traits
+#'
+#' @examples
+#' \dontrun{
+#' con <- brapi_db()$testserver
+#' con[["token"]] <- "YYYY"
+#' additionalInfo <- list(dummyData = "TRUE",
+#'                        example = "post_traits")
+#' alternativeAbbreviations <- c("PHght", "Hght")
+#' attribute <- "heigth"
+#' entity <- "plant"
+#' externalReferences <-
+#'   data.frame(referenceID = c("doi:10.155454/12341234",
+#'                              "http://purl.obolibrary.org/obo/ro.owl",
+#'                              "75a50e76"),
+#'              referenceSource = c("DOI",
+#'                                  "OBO Library",
+#'                                  "Remote Data Collection Upload Tool"))
+#' mainAbbreviation <- "PH"
+#' ontologyReference <- list(
+#'   documentationLinks = data.frame(
+#'     URL = c("http://purl.obolibrary.org/obo/ro.owl",
+#'             "https://www.cropontology.org/terms/CO_323:0000024/"),
+#'     type = c("OBO",
+#'              "WEBPAGE")),
+#'   ontologyDbId = "ontology_variable1",
+#'   ontologyName = "Ontology.org",
+#'   version = "17")
+#' status <- "recommended"
+#' synonyms <- c("Height",
+#'               "Plant Height")
+#' traitClass <- "agrinomically"
+#' traitDescription <- "The height of plant from ground."
+#' traitName <- "Plant heigth"
+#' ## Add the new trait
+#' out <- brapi_post_traits(
+#'   con = con,
+#'   additionalInfo = additionalInfo,
+#'   alternativeAbbreviations = alternativeAbbreviations,
+#'   attribute = attribute,
+#'   entity = entity,
+#'   externalReferences = externalReferences,
+#'   mainAbbreviation = mainAbbreviation,
+#'   ontologyReference = ontologyReference,
+#'   status = status,
+#'   synonyms = synonyms,
+#'   traitClass = traitClass,
+#'   traitDescription = traitDescription,
+#'   traitName = traitName)
+#' ## Obtain the identifier for the new trait
+#' traitDbId <- unique(out$traitDbId)
+#' ## Retrieve the info of the new trait
+#' brapi_get_traits_traitDbId(con = con, traitDbId)
+#' ## Update the info of the new trait
+#' brapi_put_traits_traitDbId(
+#'   con = con,
+#'   traitDbId = traitDbId,
+#'   attribute = "Height",
+#'   synonyms = c("Height",
+#'                 "Plant Height",
+#'                 "Stalk Height",
+#'                 "Canopy Height"),
+#'   traitClass = "agronomical",
+#'   traitDescription = "The height of plant from ground to the top part.",
+#'   traitName = "Plant height")
+#' }
+#'
+#' @export
+brapi_put_traits_traitDbId <- function(con = NULL,
+                                       traitDbId = '',
+                                       additionalInfo = list(),
+                                       alternativeAbbreviations = '',
+                                       attribute = '',
+                                       entity = '',
+                                       externalReferences = '',
+                                       mainAbbreviation = '',
+                                       ontologyReference = list(),
+                                       status = '',
+                                       synonyms = '',
+                                       traitClass = '',
+                                       traitDescription = '',
+                                       traitName = '') {
+  ## Create a list of used arguments
+  usedArgs <- brapirv2:::brapi_usedArgs(origValues = FALSE)
+  ## Check if BrAPI server can be reached given the connection details
+  brapi_checkCon(con = usedArgs[["con"]], verbose = FALSE)
+  ## Check validity of used and required arguments
+  brapirv2:::brapi_checkArgs(usedArgs, reqArgs = "traitDbId")
+  ## Obtain the call url
+  callurl <- brapirv2:::brapi_PUT_callURL(usedArgs = usedArgs,
+                                          callPath = "/traits/{traitDbId}",
+                                          reqArgs = "traitDbId",
+                                          packageName = "BrAPI-Phenotyping",
+                                          callVersion = 2.0)
+  ## Build the Body
+  callbody <- brapirv2:::brapi_PUT_callBody(usedArgs = usedArgs,
+                                            reqArgs = "traitDbId")
+
+  try({
+    ## Make the call and receive the response
+    resp <- brapirv2:::brapi_PUT(url = callurl, body = callbody, usedArgs = usedArgs)
+    ## Extract the content from the response object in human readable form
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    ## Convert the content object into a data.frame
+    out <- brapirv2:::brapi_result2df(cont, usedArgs)
+  })
+  ## Set class of output
+  class(out) <- c(class(out), "brapi_put_traits_traitDbId")
+  ## Show pagination information from metadata
+  brapirv2:::brapi_serverinfo_metadata(cont)
+  return(out)
+}
