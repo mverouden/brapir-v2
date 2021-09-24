e2126c97024ab0b84373f0b86a8b0246ec0647cf
Verouden, Maikel <maikel.verouden@gmail.com>
2021-09-24 13:24:04 +0200

    added tokenized pagination to brapi_serverinfo_pagination() for debugging purposes

--- R/brapi_serverinfo_pagination.R
+++ R/brapi_serverinfo_pagination.R
@@ -14,7 +14,7 @@ brapi_serverinfo_pagination <- function(cont) {
     return()
   } else {
     pagination <- contList[["metadata"]][["pagination"]]
-    if (!is.null(pagination)) {
+    if (!is.null(pagination) && !all(c("nextPageToken", "prevPageToken") %in% names(pagination))) {
       brapirv2:::brapi_message(msg = paste0("Returning page ",
                                           pagination[["currentPage"]],
                                           " (max. ",
@@ -26,6 +26,23 @@ brapi_serverinfo_pagination <- function(cont) {
                                           " items (out of a total of ",
                                           pagination[["totalCount"]],
                                           ")."))
+    } else if (!is.null(pagination) &&
+               all(c("nextPageToken", "prevPageToken") %in% names(pagination))) {
+      ## Tokenized Pagination
+      brapirv2:::brapi_message(msg = paste0("Returning page ",
+                                            pagination[["currentPage"]],
+                                            " (max. ",
+                                            ifelse(as.integer(pagination[["totalPages"]]) == 0,
+                                                   0,
+                                                   as.integer(pagination[["totalPages"]]) - 1),
+                                            ") with max. ",
+                                            pagination[["pageSize"]],
+                                            " items (out of a total of ",
+                                            pagination[["totalCount"]],
+                                            "). Next page token: ",
+                                            pagination[["nextPageToken"]],
+                                            ", previous page token: ",
+                                            pagination[["previousPageToken"]]))
     }
   }
 }
