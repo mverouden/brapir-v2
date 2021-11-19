652e518981cb8a7d228c577442e6d40b38f2fce1
Verouden, Maikel <maikel.verouden@gmail.com>
2021-11-19 18:55:34 +0100

    added brapi_post_search_variantsets() and brapi_get_search_variantsets_searchResultsDbId() with documentation and examples

---
+++ R/brapi_post_search_variantsets.R
@@ -0,0 +1,112 @@
+#' @title
+#' post /search/variantsets
+#'
+#' @description
+#' Submit a search request for `VariantSets`
+#'
+#' @param con list; required: TRUE; BrAPI connection object
+#' @param callSetDbIds vector of type character; required: FALSE; The `CallSets`
+#'    to search as specified by the unique database identifier(s); default:
+#'    &quot;&quot;, when using multiple values supply as c(&quot;value1&quot;,
+#'    &quot;value2&quot;).
+#' @param studyDbIds vector of type character; required: FALSE; List of unique
+#'    database study identifiers to search for; default: &quot;&quot;, when
+#'    using multiple values supply as c(&quot;value1&quot;, &quot;value2&quot;).
+#' @param studyNames vector of type character; required: FALSE; List of study
+#'    names to filter search results; default: &quot;&quot;, when using multiple
+#'    values supply as c(&quot;value1&quot;, &quot;value2&quot;).
+#' @param variantDbIds vector of type character; required: FALSE; The unique
+#'    Variant database identifiers to search.; default: &quot;&quot;, when using
+#'    multiple values supply as c(&quot;value1&quot;, &quot;value2&quot;).
+#' @param variantSetDbIds vector of type character; required: FALSE; The unique
+#'    `VariantSet` database identifiers to search.; default: &quot;&quot;, when
+#'    using multiple values supply as c(&quot;value1&quot;, &quot;value2&quot;).
+#' @param page integer; required: FALSE; Used to request a specific page of data
+#'    to be returned. The page indexing starts at 0 (the first page is
+#'    `page = 0`). Default is `0`.
+#' @param pageSize integer; required: FALSE; The size of the pages to be
+#'    returned. Default is `1000`.
+#'
+#' @details Submit a search request for `VariantSets`. Function will return
+#'    either the search results (Status 200 for an immediate response) or a
+#'    `searchResultsDbId` (Status 202 for both a saved and an asynchronous
+#'    search).
+#'
+#' @return data.frame
+#'
+#' @author Maikel Verouden
+#'
+#' @references \href{https://app.swaggerhub.com/apis/PlantBreedingAPI/BrAPI-Genotyping/2.0#/Variant%20Sets/post_search_variantsets }{BrAPI SwaggerHub}
+#'
+#' @family brapi-genotyping
+#' @family Variant Sets
+#'
+#' @examples
+#' \dontrun{
+#' con <- brapi_db()$testserver
+#' con[["token"]] <- "YYYY"
+#'
+#' # Immediate Response Example
+#' brapi_post_search_variantsets(con = con,
+#'                               page = 0,
+#'                               pageSize = 1000)
+#'
+#' # Saved or Asynchronous Search Response Example
+#' brapi_post_search_variantsets(con = con,
+#'                               studyDbIds = c("study1",
+#'                                              "study2"),
+#'                               studyNames = "Paw paw 2013 yield trial",
+#'                               variantSetDbIds = c("variantset1",
+#'                                                   "variantset2"),
+#'                               page = 0,
+#'                               pageSize = 1000)
+#' }
+#'
+#' @export
+brapi_post_search_variantsets <- function(con = NULL,
+                                          callSetDbIds = '',
+                                          studyDbIds = '',
+                                          studyNames = '',
+                                          variantDbIds = '',
+                                          variantSetDbIds = '',
+                                          page = 0,
+                                          pageSize = 1000) {
+  ## Create a list of used arguments
+  usedArgs <- brapirv2:::brapi_usedArgs(origValues = FALSE)
+  ## Check if BrAPI server can be reached given the connection details
+  brapi_checkCon(con = usedArgs[["con"]], verbose = FALSE)
+  ## Check validity of used and required arguments
+  brapirv2:::brapi_checkArgs(usedArgs, reqArgs = "")
+  ## Obtain the call url
+  callurl <- brapirv2:::brapi_POST_callURL(usedArgs = usedArgs,
+                                           callPath = "/search/variantsets",
+                                           reqArgs = "",
+                                           packageName = "BrAPI-Genotyping",
+                                           callVersion = 2.0)
+  ## Build the Body
+  callbody <- brapirv2:::brapi_POST_callBody(usedArgs = usedArgs,
+                                             reqArgs = "")
+
+  try({
+    ## Make the call and receive the response
+    resp <- brapirv2:::brapi_POST(url = callurl, body = callbody, usedArgs = usedArgs)
+    ## Message about call status
+    if (httr::status_code(resp) == 200) {
+      message(paste0("Immediate Response.", "\n"))
+    } else if (httr::status_code(resp) == 202) {
+      message(paste0("Saved or Asynchronous Response has provided a searchResultsDbId.", "\n"))
+      message(paste0("Use the GET /search/variantsets/{searchResultsDbId} call to retrieve the paginated output.", "\n"))
+    } else {
+      stop(paste0("The POST /search/variantsets call resulted in Server status, ", httr::http_status(resp)[["message"]]))
+    }
+    ## Extract the content from the response object in human readable form
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    ## Convert the content object into a data.frame
+    out <- brapirv2:::brapi_result2df(cont, usedArgs)
+  })
+  ## Set class of output
+  class(out) <- c(class(out), "brapi_post_search_variantsets")
+  ## Show pagination information from metadata
+  brapirv2:::brapi_serverinfo_metadata(cont)
+  return(out)
+}
